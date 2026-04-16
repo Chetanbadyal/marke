bf824f4f3e8b9c354c7c344a8cf66bd470da3564
Chetanbadyal <badyal1233@gmail.com>
2026-04-16 11:06:57 +0530

    Update EnquiryModel.js

--- Server/Enquiry/EnquiryModel.js
+++ Server/Enquiry/EnquiryModel.js
@@ -1,12 +1,13 @@
+const mongoose = require("mongoose");
 
-{/* const mongoose=require("mongoose")
-const EnquirySchema=new mongoose.Schema({
-    EnquiryName:{type:String,default:null},
-    Email:{type:String,default:null},
-    Subject:{type:String,default:null},
-    Message:{type:String,default:null},
-    Status:{type:String,default:'Active'},
-    createdAt:{type:Date,default:Date.now()}
-})
-module.exports=new mongoose.model('Enquiry',EnquirySchema)  */}
+const EnquirySchema = new mongoose.Schema({
+    EnquiryName: { type: String, default: null },
+    Email: { type: String, default: null },
+    Subject: { type: String, default: null },
+    Message: { type: String, default: null },
+    Status: { type: String, default: 'Active' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Enquiry', EnquirySchema);
 
